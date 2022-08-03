e761ced9e4614311984a4e1c9995902f68f5f584
Abdulaziz Buarki <aziiiiiiz_b@icloud.com>
2022-08-03 19:42:43 +0300

    cw 2

---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var favouritenumber :Int = 82
+var name:String = "aziz"
+let birthday = 2006
+var hobby = "kb"
+print ("hello my name is \(name) and my favourite number is \(favouritenumber) and my hobby is \(hobby) and i was born at \(birthday) ")
